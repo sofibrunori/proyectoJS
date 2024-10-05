3067b10d9cc01f7776e58ca89d9eb066e77e18d8
Sofía Brunori <sofiabrunoric@gmail.com>
2024-10-04 22:51:20 -0300

    html añadidos

---
+++ JS/app.js
@@ -0,0 +1,56 @@
+const Productos = [
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+    {
+        id: 1,
+        nombre: "Cuadro",
+        precio: 1000,
+        img: "",
+    },
+]
